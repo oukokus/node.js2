2b1b0716a88d2d87d36063609fa3b5e885a1954e
oku <oukokus@gmail.com>
2024-02-27 14:21:32 +0900

    レビューの更新機能 完成

--- app.js
+++ app.js
@@ -1,49 +1,45 @@
-const path = require('path');
-const express = require('express');
-const ejs = require('ejs');
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = 3000;
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const con = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'rootroot',
-  database: 'node2_db'
+  host: "127.0.0.1",
+  user: "root",
+  password: "rootroot",
+  database: "node2_db",
 });
 
 // mysqlからデータを持ってくる
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // cssファイルの取得
-app.use(express.static('assets'));
+  app.use(express.static("assets"));
   const sql = "select * from personas";
   // 参考例
   const num = 10000;
 
-
-
-
   // ==========ここまでの範囲で書くようにしましょう。==========
-  app.post('/', (req, res) => {
-    const sql = "INSERT INTO personas SET ?"
-    con.query(sql, req.body, function(err, result, fields) {
+  app.post("/", (req, res) => {
+    const sql = "INSERT INTO personas SET ?";
+    con.query(sql, req.body, function (err, result, fields) {
       if (err) throw err;
       console.log(result);
-      res.redirect('/');
+      res.redirect("/");
     });
   });
-  
-  app.get('/create', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.ejs'))
+
+  app.get("/create", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.ejs"));
   });
-  
+
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
-    res.render('index', {
-      
+    res.render("index", {
       users: result,
 
       // ⓵ こちらはapp.jsで宣言した変数をindex.ejsのscriptタグ内で使用するために登録する場所になっています。
@@ -51,40 +47,36 @@ app.use(express.static('assets'));
         指定の仕方はオブジェクトの考え方と同じで、プロパティ名: 値の形になります。値の部分は変数名を入れるようにして下さい。
         プロパティ名はindex.ejsで使用しますので、何の値が入ってるかわかるような名前にしましょう。
       */
-
-
     });
   });
 });
 
-app.get('/edit/:id', (req, res) => {
+app.get("/edit/:id", (req, res) => {
   const sql = "SELECT * FROM personas WHERE id = ?";
   con.query(sql, [req.params.id], function (err, result, fields) {
     if (err) throw err;
-    res.render('edit', {
-      user: result
+    res.render("edit", {
+      user: result,
     });
   });
 });
 
-app.post('/update/:id', (req, res) => {
+app.post("/update/:id", (req, res) => {
   const sql = "UPDATE personas SET ? WHERE id = " + req.params.id;
   con.query(sql, req.body, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
-app.get('/delete/:id', (req, res) => {
+app.get("/delete/:id", (req, res) => {
   const sql = "DELETE FROM users WHERE id = ?";
-  con.query(
-    sql, [req.params.id],
-    function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-      res.redirect('/');
-    });
+  con.query(sql, [req.params.id], function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("/");
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
